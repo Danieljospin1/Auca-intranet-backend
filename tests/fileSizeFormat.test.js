90c0796c951565bf5aca29c3452cf50a32c30174
Danieljospin1 <danieljospin087@gmail.com>
2026-01-01 13:12:22 +0200

    Fix formatting issue in fileSizeFormat test for kilobytes

--- tests/fileSizeFormat.test.js
+++ tests/fileSizeFormat.test.js
@@ -13,7 +13,7 @@ describe('formatFileSize', () => {
   test('should format kilobytes correctly', () => {
     expect(formatFileSize(1024)).toBe('1.0 KB');
     expect(formatFileSize(1536)).toBe('1.5 KB');
-  });
+  })
 
   test('should format megabytes correctly', () => {
     expect(formatFileSize(1024 * 1024)).toBe('1.0 MB');
