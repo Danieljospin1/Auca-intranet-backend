4b1c188ed904b31edca0b116a29b2a63c74415a9
Danieljospin1 <danieljospin087@gmail.com>
2026-03-22 09:03:58 +0200

    updating port

--- database & models/databaseConnection.js
+++ database & models/databaseConnection.js
@@ -7,7 +7,7 @@ const DBconnection=mysql.createPool({
   user:process.env.dbroot,
   password:process.env.dbpassword,
   database:process.env.dbname,
-  port:24831,
+  port:process.env.dbport,
   timezone: '+00:00'
   
 });
