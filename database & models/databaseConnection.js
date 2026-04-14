0baeb9675a5ee6bfd1805c8b3ff29db2aa81c417
Danieljospin1 <danieljospin087@gmail.com>
2026-04-14 19:06:22 +0200

    fix: update database connection port to 33282

--- database & models/databaseConnection.js
+++ database & models/databaseConnection.js
@@ -7,7 +7,7 @@ const DBconnection=mysql.createPool({
   user:process.env.dbroot,
   password:process.env.dbpassword,
   database:process.env.dbname,
-  port:29108,
+  port:33282,
   timezone: '+00:00'
   
 });
