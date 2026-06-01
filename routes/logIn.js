aebc55836d5fb6ac59353b7092d19d1ab5b876a7
Danieljospin1 <danieljospin087@gmail.com>
2026-06-01 22:51:30 +0200

    feat: update login logic to handle UserType and enhance authentication for aucasa users

--- routes/logIn.js
+++ routes/logIn.js
@@ -8,14 +8,19 @@ const connectionPromise = require('../database & models/databaseConnection');
 
 
 router.post('/', async (req, res) => {
-    const { Id, Password, isStaff } = req.body;
+    const { Id, Password, UserType } = req.body;
     console.log("user received.......",req.body)
     if (!Id || !Password ) {
         return res.json("message:Please input Your Id/Email And Password")
     }
+    if(!UserType){
+        return res.status(400).json("message: Unable to get user type.")
+    }
+    
+
 
     try {
-        if (isStaff === false) {
+        if (UserType.toLoweCase()==="student" ) {
             if (typeof (Id) == 'string') {
                 return res.status(401).json({"message":'input valid id'})
             }
@@ -34,8 +39,32 @@ router.post('/', async (req, res) => {
                 }
             }
         }
+        if(UserType.toLowerCase()==="aucasa"){
+            if (typeof (Id) == 'string') {
+                return res.status(401).json({"message":'input valid Id.'})
+            }
+            else {
+                const [student] = await connectionPromise.query(`select * from students where StudentId=? AND Password=?`,[Id,Password]);
 
-        if (isStaff === true) {
+                if (!student[0]) {
+                    return res.status(401).json("invalid user credentials")
+                }
+                else {
+                    const [aucasaUserRole]=await connectionPromise.query(`select role from aucasa where Id=? AND IsInService=1`,[Id]);
+                    if(!aucasaUserRole){
+                        return res.status(401).json("invalid user credentials");
+                    }
+                    const [studentProfile]= await connectionPromise.query(`select StudentId,Fname,Lname,Email,Phone,Faculty,Department,ProfileUrl,StudyLevel from students where StudentId=?`,[Id]);
+                    const studentFaculty = await connectionPromise.query(`SELECT Faculty FROM students WHERE StudentId=?`,[Id])
+                    const accessToken = token.sign({ "Id": Id,"StudyLevel": studentProfile[0].StudyLevel, "Faculty": studentFaculty[0][0].Faculty,"Department": studentProfile[0].Department, "role": "student","aucasaUserRole":aucasaUserRole[0].role }, process.env.ACCESS_TOKEN_SECRET, {})
+                    const refreshToken = token.sign({ "Id": Id,"StudyLevel": studentProfile[0].StudyLevel, "Faculty": studentFaculty[0][0].Faculty,"Department":studentProfile[0].Department, "role": "student","aucasaUserRole":aucasaUserRole[0].role }, process.env.REFRESH_TOKEN_SECRET, { expiresIn: '15d' })
+                    return res.status(200).send({ accessToken, refreshToken,studentProfile,aucasaUserRole });
+                }
+            }
+
+        }
+
+        if (UserType.toLowerCase() === "staff") {
             const [staffId] = await connectionPromise.query(`select Id from staff where Email=? AND Password=?`,[Id,Password])
 
             if (!staffId[0]) {
